a920122bda2d64be17d2c39174a7d9ec0d8a57d9
RaghavIsCoding <142236771+RaghavIsCoding@users.noreply.github.com>
2023-08-25 23:33:41 +0530

    Create indian.py

---
+++ indian.py
@@ -0,0 +1,9 @@
+import time
+i = 100
+for i in range(1, 101):
+    None
+while (i > 0):
+    print(f"{i}-Happy Indenpendence day | Happy ISRO | Happy Modi | Happy India\n")
+    i = i - 1
+    time.sleep(0.3)
+print("\nDONE")
